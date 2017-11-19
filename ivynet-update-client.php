7b950f46055ed47cab047ddd0ff387d11cf0d85f
changwoo <ep6tri@hotmail.com>
2017-11-19 18:59:42 +0900

    1.0.0 업데이트.

--- ivynet-update-client.php
+++ ivynet-update-client.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Ivynet Update Client
  * Description: 아이비넷의 플러그인 업데이트 체크를 위한 확장 플러그인
- * Version:     0.0.0
+ * Version:     1.0.0
  * Author:      Ivynet
  * Author URI:  https://ivynet.kr
  * License:     GPLv2+
@@ -12,15 +12,40 @@ define( 'IUC_MAIN', __FILE__ );
 define( 'IUC_DIR', __DIR__ );
 define( 'IUC_VERSION', '0.0.0' );
 
+add_action( 'load-plugins.php', 'iuc_before_update_plugins', 5 );
+add_action( 'load-update.php', 'iuc_before_update_plugins', 5 );
+add_action( 'load-update-core.php', 'iuc_before_update_plugins', 5 );
+add_action( 'wp_update_plugins', 'iuc_before_update_plugins', 5 );
+
 add_action( 'load-plugins.php', 'iuc_update_plugins', 100 );
 add_action( 'load-update.php', 'iuc_update_plugins', 100 );
 add_action( 'load-update-core.php', 'iuc_update_plugins', 100 );
 add_action( 'wp_update_plugins', 'iuc_update_plugins', 100 );
 
+/** @var int $update_last_checked */
+$update_last_checked = 0;
+
+function iuc_before_update_plugins() {
+
+    global $update_last_checked;
+
+    $update_plugins = get_site_transient( 'update_plugins' );
+    if ( isset( $update_plugins->last_checked ) ) {
+        $update_last_checked = $update_plugins->last_checked;
+    }
+}
+
+/**
+ * @see   wp_update_plugins()
+ *
+ * @param array $extra_stats
+ */
 function iuc_update_plugins( $extra_stats = array() ) {
     if ( wp_installing() ) {
         return;
     }
+
+    global $update_last_checked;
 
     // include an unmodified $wp_version
     /**
@@ -36,9 +61,6 @@ function iuc_update_plugins( $extra_stats = array() ) {
     if ( ! is_object( $current ) ) {
         $current = new stdClass();
     }
-
-    $new_option               = new stdClass;
-    $new_option->last_checked = time();
 
     $doing_cron = wp_doing_cron();
 
@@ -62,13 +84,11 @@ function iuc_update_plugins( $extra_stats = array() ) {
             }
     }
 
-    $time_not_changed = isset( $current->last_checked ) && $timeout > ( time() - $current->last_checked );
-    $time_not_changed = FALSE; // 개발중.
+    $time_not_changed = isset( $current->last_checked ) && $timeout > ( time() - $update_last_checked );
+
     if ( $time_not_changed && ! $extra_stats ) {
         $plugin_changed = FALSE;
         foreach ( $plugins as $file => $p ) {
-            $new_option->checked[ $file ] = $p['Version'];
-
             if ( ! isset( $current->checked[ $file ] ) || strval( $current->checked[ $file ] ) !== strval( $p['Version'] ) ) {
                 $plugin_changed = TRUE;
             }
@@ -113,8 +133,7 @@ function iuc_update_plugins( $extra_stats = array() ) {
     }
 
     $options = array(
-//        'timeout'    => $timeout,
-        'timeout'    => -1,
+        'timeout'    => $timeout,
         'body'       => array(
             'plugins'      => wp_json_encode( $to_send ),
             'translations' => wp_json_encode( $translations ),
@@ -122,56 +141,53 @@ function iuc_update_plugins( $extra_stats = array() ) {
             'all'          => wp_json_encode( TRUE ),
         ),
         'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' ),
-        'cookies'    => array( 'XDEBUG_SESSION' => 'phpstorm-xdebug' ),
     );
 
     if ( $extra_stats ) {
         $options['body']['update_stats'] = wp_json_encode( $extra_stats );
     }
 
-    $url = $http_url = 'http://wp48.local/plugins/update-check/1.0/';
+    $url = $http_url = apply_filters( 'iuc_update_url', 'http://update.wpkorea.org/plugins/update-check/1.0/' );
 //    if ( $ssl = wp_http_supports( array( 'ssl' ) ) ) {
 //        $url = set_url_scheme( $url, 'https' );
 //    }
 
     $raw_response = wp_remote_post( $url, $options );
-    if ( $ssl && is_wp_error( $raw_response ) ) {
-        trigger_error( sprintf(/* translators: %s: support forums URL */
-                           __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="%s">support forums</a>.' ),
-                           __( 'https://wordpress.org/support/' ) ) . ' ' . __( '(WordPress could not establish a secure connection to WordPress.org. Please contact your server administrator.)' ),
-            headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE );
-        $raw_response = wp_remote_post( $http_url, $options );
-    }
+//    if ( $ssl && is_wp_error( $raw_response ) ) {
+//        $raw_response = wp_remote_post( $http_url, $options );
+//    }
 
     if ( is_wp_error( $raw_response ) || 200 != wp_remote_retrieve_response_code( $raw_response ) ) {
         return;
     }
 
-    $response = json_decode( wp_remote_retrieve_body( $raw_response ), TRUE );
-    foreach ( $response['plugins'] as &$plugin ) {
-        $plugin = (object) $plugin;
-        if ( isset( $plugin->compatibility ) ) {
-            $plugin->compatibility = (object) $plugin->compatibility;
-            foreach ( $plugin->compatibility as &$data ) {
-                $data = (object) $data;
-            }
-        }
-    }
-    unset( $plugin, $data );
-    foreach ( $response['no_update'] as &$plugin ) {
-        $plugin = (object) $plugin;
-    }
-    unset( $plugin );
+    $ius_response   = json_decode( wp_remote_retrieve_body( $raw_response ), TRUE );
+    $update_plugins = get_site_transient( 'update_plugins' );
 
-    if ( is_array( $response ) ) {
-        $new_option->response     = $response['plugins'];
-        $new_option->translations = $response['translations'];
-        $new_option->no_update    = $response['no_update'];
-    } else {
-        $new_option->response     = array();
-        $new_option->translations = array();
-        $new_option->no_update    = array();
+    $update_plugins->last_checked = time();
+
+    foreach ( $ius_response['response'] as $main_file => $plugin_data ) {
+        $update_plugins->response[ $main_file ] = (object) $plugin_data;
     }
 
-    // set_site_transient( 'update_plugins', $new_option );
+    set_site_transient( 'update_plugins', $update_plugins );
+}
+
+
+add_filter( 'http_request_host_is_external', 'iuc_accept_local_domain', 10, 2 );
+
+/**
+ * Test callback filter
+ *
+ * @param $allow
+ * @param $domain
+ *
+ * @return bool
+ */
+function iuc_accept_local_domain( $allow, $domain ) {
+    if ( $domain === 'update.local' ) {
+        $allow = TRUE;
+    }
+
+    return $allow;
 }
